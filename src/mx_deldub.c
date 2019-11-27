6e3dd09135c1cd74ff49d766dc9b85ec7363e4c2
KozakMiksa <kozak.miksa@gmail.com>
2019-11-27 18:47:55 +0200

    auditor DONE

--- src/mx_deldub.c
+++ src/mx_deldub.c
@@ -1,12 +1,9 @@
 #include "path.h"
 
-static int arrlen(char **arr)
-{
+static int arrlen(char **arr) {
     int count = 0;
     while(arr[count] != NULL)
-    {
     	count++;
-    }
     return count;
 }
 
@@ -17,21 +14,15 @@ char **mx_deldub(char **arr1)
     int len = mx_strlen(arr1[0]);
     arr2[0] = mx_strnew(len);
     for (int n = 0; n < len; n++)
-    	{
-    		arr2[0][n] = arr1[0][n];
-    	}
-    for (int i = 1; arr1[i] != NULL; i++)
-    {
+    	arr2[0][n] = arr1[0][n];
+    for (int i = 1; arr1[i] != NULL; i++) {
     	int j = 0;
     	for (; mx_strcmp(arr1[i], arr2[j]) != 0 && j < c - 1; j++);
-    	if (j == c - 1 && mx_strcmp(arr1[i], arr2[j]) != 0)
-    	{
+    	if (j == c - 1 && mx_strcmp(arr1[i], arr2[j]) != 0) {
     		len = mx_strlen(arr1[i]);
     		arr2[c] = mx_strnew(len);
-    		for (int n = 0; n < len; n++) 
-    		{
+    		for (int n = 0; n < len; n++)
     			arr2[c][n] = arr1[i][n];
-    		}
     		c++;
     	}
     }
--- src/mx_printerror.c
+++ src/mx_printerror.c
@@ -1,90 +1,66 @@
 #include "path.h"
 
-static void error_1_2_3(int num, char *file)
-{
-    if (num != 2)
-    {
+static void error_1_2_3(int num, char *f) {
+    if (num != 2) {
         write(2,"usage: ./pathfinder [filename]\n", 31);
         exit(0);
     }
-    int fd = open(file, O_RDONLY);
-    if (0 > fd)
-    {
-    char *str = mx_strjoin(mx_strjoin("file ", file), " does not exist\n");
+    int fd = open(f, O_RDONLY);
+    if (0 > fd) {
+        char *ss = mx_strjoin("error: file ", f);
+        char *s = mx_strjoin(ss, " does not exist\n");
         close(fd);
-        write(2, str, mx_strlen(str));
+        write(2, s, mx_strlen(s));
         exit(0);
     }
     char buf;
     int c = read(fd, &buf, 1);
-    if (c <= 0)
-    {
-    char *s = mx_strjoin(mx_strjoin("error: file ", file), " is empty\n");
+    if (c <= 0) {
+        char *s = mx_strjoin(mx_strjoin("error: file ", f), " is empty\n");
         close(fd);
         write(2, s, mx_strlen(s));
         exit(0);
     }
 }
 
-static void printerror_5(int i)
-{
+static void printerror_5(int i) {
     char *s = mx_strjoin("error: line ", mx_itoa(i + 1));
     char *ss = mx_strjoin(s, " isn't valid\n");
     write(2, ss, mx_strlen(ss));
     exit(0);
 }
 
-static void error_4_5(char **arr)
-{
-    for(int i = 0; arr[0][i] != '\0'; i++)
-    {
-        if (mx_isdigit(arr[0][i]) == 0)
-        {
+static void error_4_5(char **arr) {
+    for (int i = 0; arr[0][i] != '\0'; i++) {
+        if (mx_isdigit(arr[0][i]) == 0) {
             write(2, "error: line 1 isn't valid\n", 26);
             exit(0);
         }
     }
-    for(int i = 1; arr[i] != NULL; i++)
-    {
+    for (int i = 1; arr[i] != NULL; i++) {
         int j = 0;
-        for(; arr[i][j] != '-'; j++)
-        {
+        for (; arr[i][j] != '-'; j++)
             if (mx_isalpha(arr[i][j]) == 0)
-            {
                 printerror_5(i);
-            }
-        }
         j++;
-        for(; arr[i][j] != ','; j++)
-        {
+        for (; arr[i][j] != ','; j++)
             if (mx_isalpha(arr[i][j]) == 0)
-            {
                 printerror_5(i);
-            }
-        }
         j++;
-        for(; arr[i][j] != '\0'; j++)
-        {
+        for (; arr[i][j] != '\0'; j++)
             if (mx_isdigit(arr[i][j]) == 0)
-            {
                 printerror_5(i);
-            }
-        }
     }   
 }
 
-static char *arr_to_str(char **arr, char *str_new)
-{
+static char *arr_to_str(char **arr, char *str_new) {
     char *str = mx_strnew(mx_strlen(str_new));
     mx_strdel(&str_new);
     int s = 0;
-    for(int i = 1; arr[i] != NULL; i++)
-    {
-        for(int j = 0; arr[i][j] != ',';)
-        {
+    for (int i = 1; arr[i] != NULL; i++) {
+        for (int j = 0; arr[i][j] != ',';) {
             str[s++] = arr[i][j++];
-            if (mx_isalpha(arr[i][j]) != 1)
-            {
+            if (mx_isalpha(arr[i][j]) != 1) {
                 str[s++] = ',';
                 if (arr[i][j] != ',')
                     j++;
@@ -94,8 +70,7 @@ static char *arr_to_str(char **arr, char *str_new)
     return str;
 }
 
-void mx_printerror(int argc, char *argv)
-{
+void mx_printerror(int argc, char *argv) {
     error_1_2_3(argc, argv);
     char *str = mx_file_to_str(argv);
     char **arr = mx_strsplit(str, '\n');
@@ -104,12 +79,12 @@ void mx_printerror(int argc, char *argv)
     str = arr_to_str(arr, str);
     mx_del_strarr(&arr);
     arr = mx_strsplit(str, ',');
+    mx_strdel(&str);
     char **err6 = mx_deldub(arr);
     mx_del_strarr(&arr);
     int i = 0;
     for(; err6[i] != NULL; i++);
-    if (i != island_size)
-    {
+    if (i != island_size) {
         write(2, "error: invalid number of islands\n", 33);
         exit(0);
     }
